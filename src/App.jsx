a03f0afc5b5d3c4b72cbc837345f0a1d2dfc538e
Nikita Filin <nikita10filin@gmail.com>
2025-03-26 15:47:15 +0200

    layout mode added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 
 import './App.css'
 import Navbar from "@/components/ui/Navbar/Navbar.jsx";
+import { ThemeProvider } from "@/components/teme-provider.jsx"
+import {ModeToggle} from "@/components/mode-toggle.jsx";
 
 
 function App() {
 
   return (
     <>
-        <Navbar/>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <Navbar/>
+            <ModeToggle/>
+        </ThemeProvider>
 
     </>
   )
